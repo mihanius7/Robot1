cbdc2ff973e99b20edc029a432e22848952d9bf9
Mihas Dainiak <mihanius@gmail.com>
2022-06-02 13:49:38 +0300

    ObjectBox class

--- src/edu/javacourse/robot/datastructures/ObjectQueue.java
+++ src/edu/javacourse/robot/datastructures/ObjectQueue.java
@@ -1,15 +1,81 @@
 package edu.javacourse.robot.datastructures;
 
-public class ObjectQueue {
-	public void push(Object obj) {
-		
-	}
-	
-	public Object pull() {
-		return null;
-	}
-	
-	public int size() {
-		return 0;
-	}
+public class ObjectQueue
+{
+    // Указатель на первый элемент
+    private ObjectBox head = null;
+    // Указатель на последний элемент
+    private ObjectBox tail = null;
+    // Поле для хранения размера очереди
+    private int size = 0;
+
+    public void push(Object obj) {
+        // Сразу создаем вспомогательный объект и помещаем новый элемент в него
+        ObjectBox ob = new ObjectBox();
+        ob.setObject(obj);
+        // Если очередь пустая - в ней еще нет элементов
+        if (head == null) {
+            // Теперь наша голова указывает на наш первый элемент
+            head = ob;
+        } else {
+            // Если это не первый элемент, то надо, чтобы последний элемент в очереди
+            // указывал на вновь прибывший элемент
+            tail.setNext(ob);
+        }
+        // И в любом случае нам надо наш "хвост" переместить на новый элемент
+        // Если это первый элемент, то "голова" и "хвост" будут указывать на один и тот же элемент
+        tail = ob;
+        // Увеличиваем размер нашей очереди
+        size++;
+    }
+
+    public Object pull() {
+        // Если у нас нет элементов, то возвращаем null
+        if (size == 0) {
+            return null;
+        }
+        // Получаем наш объект из вспомогательного класса из "головы"
+        Object obj = head.getObject();
+        // Перемещаем "голову" на следующий элемент
+        head = head.getNext();
+        // Если это был единственный элемент, то head станет равен null
+        // и тогда tail (хвост) тоже дожен указать на null.
+        if (head == null) {
+            tail = null;
+        }
+        // Уменьшаем размер очереди
+        size--;
+        // Возвращаем значение
+        return obj;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    // Наш вспомогательный класс будет закрыт от посторонних глаз
+    private class ObjectBox
+    {
+        // Поле для хранения объекта
+        private Object object;
+        // Поле для указания на следующий элемент в цепочке.
+        // Если оно равно NULL - значит это последний элемент
+        private ObjectBox next;
+
+        public Object getObject() {
+            return object;
+        }
+
+        public void setObject(Object object) {
+            this.object = object;
+        }
+
+        public ObjectBox getNext() {
+            return next;
+        }
+
+        public void setNext(ObjectBox next) {
+            this.next = next;
+        }
+    }
 }
--- src/edu/javacourse/robot/datastructures/QueueTest.java
+++ src/edu/javacourse/robot/datastructures/QueueTest.java
@@ -3,11 +3,11 @@ package edu.javacourse.robot.datastructures;
 public class QueueTest {
 	public static void main(String[] arg) {
 		ObjectQueue queue = new ObjectQueue();
-		
+
 		for (int i = 0; i < 10; i++) {
 			queue.push("Text: " + i);
 		}
-		
+
 		while (queue.size() > 0) {
 			String s = (String) queue.pull();
 			System.out.println(s);
