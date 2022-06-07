3763238b377ec4beef174d2b14b014805cf6e528
Mihas Dainiak <mihanius@gmail.com>
2022-06-07 12:35:23 +0300

    Comments removed. Previous object field added.

--- src/edu/javacourse/robot/datastructures/ObjectQueue.java
+++ src/edu/javacourse/robot/datastructures/ObjectQueue.java
@@ -2,70 +2,45 @@ package edu.javacourse.robot.datastructures;
 
 public class ObjectQueue
 {
-    // Указатель на первый элемент
     private ObjectBox head = null;
-    // Указатель на последний элемент
     private ObjectBox tail = null;
-    // Поле для хранения размера очереди
     private int size = 0;
 
     public void push(Object obj) {
-        // Сразу создаем вспомогательный объект и помещаем новый элемент в него
         ObjectBox ob = new ObjectBox();
         ob.setObject(obj);
-        // Если очередь пустая - в ней еще нет элементов
         if (head == null) {
-            // Теперь наша голова указывает на наш первый элемент
             head = ob;
         } else {
-            // Если это не первый элемент, то надо, чтобы последний элемент в очереди
-            // указывал на вновь прибывший элемент
             tail.setNext(ob);
         }
-        // И в любом случае нам надо наш "хвост" переместить на новый элемент
-        // Если это первый элемент, то "голова" и "хвост" будут указывать на один и тот же элемент
         tail = ob;
-        // Увеличиваем размер нашей очереди
         size++;
     }
 
     public Object pull() {
-        // Если у нас нет элементов, то возвращаем null
         if (size == 0) {
             return null;
         }
-        // Получаем наш объект из вспомогательного класса из "головы"
         Object obj = head.getObject();
-        // Перемещаем "голову" на следующий элемент
         head = head.getNext();
-        // Если это был единственный элемент, то head станет равен null
-        // и тогда tail (хвост) тоже дожен указать на null.
         if (head == null) {
             tail = null;
         }
-        // Уменьшаем размер очереди
         size--;
-        // Возвращаем значение
         return obj;
     }
 
     public Object get(int index) {
-        // Если нет элементов или индекс больше размера или индекс меньше 0
         if(size == 0 || index >= size || index < 0) {
             return null;
         }
-        // Устанавлваем указатель, который будем перемещать на "голову"
         ObjectBox current = head;
-        // В этом случае позиция равну 0
         int pos = 0;
-        // Пока позиция не достигла нужного индекса
         while(pos < index) {
-            // Перемещаемся на следующий элемент
             current = current.getNext();
-            // И увеличиваем позицию
             pos++;
         }
-        // Мы дошли до нужной позиции и теперь можем вернуть элемент
         Object obj = current.getObject();
         return obj;
     }
@@ -74,16 +49,13 @@ public class ObjectQueue
         return size;
     }
 
-    // Наш вспомогательный класс будет закрыт от посторонних глаз
     private class ObjectBox
     {
-        // Поле для хранения объекта
         private Object object;
-        // Поле для указания на следующий элемент в цепочке.
-        // Если оно равно NULL - значит это последний элемент
         private ObjectBox next;
+        private ObjectBox prev;
 
-        public Object getObject() {
+		public Object getObject() {
             return object;
         }
 
@@ -98,5 +70,13 @@ public class ObjectQueue
         public void setNext(ObjectBox next) {
             this.next = next;
         }
+        
+        public ObjectBox getPrev() {
+			return prev;
+		}
+
+		public void setPrev(ObjectBox prev) {
+			this.prev = prev;
+		}
     }
 }
