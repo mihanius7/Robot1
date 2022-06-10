35b8c07279dfac6a1e333631b9e6c1588b56c59a
Mihas Dainiak <mihanius@gmail.com>
2022-06-10 10:16:44 +0300

    Using SimpleRobotListener

--- src/edu/javacourse/robot/RobotManager.java
+++ src/edu/javacourse/robot/RobotManager.java
@@ -5,19 +5,18 @@ import javax.swing.JFrame;
 
 public class RobotManager {
 	public static void main(String[] args) {
-		// Количество сторон многоугольника
-		final int COUNT = 12;
-		// Длина стороны
+		final int COUNT = 5;
 		final int SIDE = 100;
-
+		
 		Robot robot = new Robot(200, 50);
-		// Создаем замкнутую фигуру с количеством углов COUNT
+		RobotListener listener = new SimpleRobotListener();
+		robot.setListener(listener);
+		
 		for (int i = 0; i < COUNT; i++) {
 			robot.forward(SIDE);
 			robot.setCourse(robot.getCourse() + 360 / COUNT);
 		}
 
-		// Создаем форму для отрисовки пути нашего робота
 		RobotFrame rf = new RobotFrame(robot);
 		rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		rf.setVisible(true);
