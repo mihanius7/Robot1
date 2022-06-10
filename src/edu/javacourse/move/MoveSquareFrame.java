648cbcfa96962cdbff59a34badd312c4a8f3c15f
Mihas Dainiak <mihanius@gmail.com>
2022-06-10 10:42:37 +0300

    MoveSquareFrame class writing

--- src/edu/javacourse/move/MoveSquareFrame.java
+++ src/edu/javacourse/move/MoveSquareFrame.java
@@ -1,7 +1,27 @@
 package edu.javacourse.move;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
 public class MoveSquareFrame extends JFrame {
+	public static final String UP = "UP";
+	public static final String DOWN = "DOWN";
 
+	public MoveSquareFrame() {
+		SquareComponent sc = new SquareComponent();
+		add(sc);
+
+		JButton buttonUp = new JButton(UP);
+		buttonUp.setActionCommand(UP);
+		buttonUp.addActionListener(sc);
+		add(buttonUp, BorderLayout.NORTH);
+
+		JButton buttonDown = new JButton(DOWN);
+		buttonDown.setActionCommand(DOWN);
+		buttonDown.addActionListener(sc);
+		add(buttonUp, BorderLayout.SOUTH);
+
+		setBounds(100, 100, 400, 400);
+	}
 }
