ad03989621bf1acfc2c3ae08b2c81a3029746f01
Mihas Dainiak <mihanius@gmail.com>
2022-06-10 10:50:29 +0300

    MoveSquare main

---
+++ src/edu/javacourse/move/MoveSquare.java
@@ -0,0 +1,11 @@
+package edu.javacourse.move;
+
+import javax.swing.JFrame;
+
+public class MoveSquare {
+	public static void main(String[] args) {
+		MoveSquareFrame frame = new MoveSquareFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
