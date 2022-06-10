0c900e7765f1dd927fc4655502f955b2b59e3abb
Mihas Dainiak <mihanius@gmail.com>
2022-06-10 10:10:00 +0300

    Using RobotListener

--- src/edu/javacourse/robot/Robot.java
+++ src/edu/javacourse/robot/Robot.java
@@ -2,53 +2,57 @@ package edu.javacourse.robot;
 
 import edu.javacourse.robot.datastructures.ObjectQueue;
 
-public class Robot
-{
-    private double x = 0;
-    private double y = 0;
-    protected double course = 0;
-    // Список для хранения линий, по которым перемещался робот
-    // Пока будем использовать его без подробностей
-    private ObjectQueue lines = new ObjectQueue();
+public class Robot {
+	private double x = 0;
+	private double y = 0;
+	protected double course = 0;
+	private ObjectQueue lines = new ObjectQueue();
+	private RobotListener listener;
 
-    public Robot(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-    
-    public void printCoordinates() {
-    	System.out.println("x = " + x + ", y = " + y);
-    }
+	public Robot(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    public void forward(int distance) {
-        // Запоминаем координаты робота перед перемещением
-        final double xOld = x;
-        final double yOld = y;
-        // Меняем координаты
-        x += distance * Math.cos(course / 180 * Math.PI);
-        y += distance * Math.sin(course / 180 * Math.PI);
-        // Запоминаем координаты пройденного пути в списке
-        // Класс List позволяет добавить объект и хранить его
-        lines.push(new RobotLine(xOld, yOld, x, y));
-    }
+	public void setListener(RobotListener listener) {
+		this.listener = listener;
+	}
 
-    public double getX() {
-        return x;
-    }
+	public void printCoordinates() {
+		System.out.println("x = " + x + ", y = " + y);
+	}
 
-    public double getY() {
-        return y;
-    }
+	public void forward(int distance) {
+		if (listener != null) {
+			listener.startMove(x, y);
+		}
+		final double xOld = x;
+		final double yOld = y;
+		x += distance * Math.cos(course / 180 * Math.PI);
+		y += distance * Math.sin(course / 180 * Math.PI);
+		if (listener != null) {
+			listener.endMove(x, y);
+		}
+		lines.push(new RobotLine(xOld, yOld, x, y));
+	}
 
-    public double getCourse() {
-        return course;
-    }
+	public double getX() {
+		return x;
+	}
 
-    public void setCourse(double course) {
-        this.course = course;
-    }
+	public double getY() {
+		return y;
+	}
 
-    public ObjectQueue getLines() {
-        return lines;
-    }
+	public double getCourse() {
+		return course;
+	}
+
+	public void setCourse(double course) {
+		this.course = course;
+	}
+
+	public ObjectQueue getLines() {
+		return lines;
+	}
 }
