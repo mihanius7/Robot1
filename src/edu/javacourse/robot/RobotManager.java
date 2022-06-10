bc19bda50149453d2f5130e371725e5910cd6eb4
Mihas Dainiak <mihanius@gmail.com>
2022-06-10 10:20:57 +0300

    Coordinates changed

--- src/edu/javacourse/robot/RobotManager.java
+++ src/edu/javacourse/robot/RobotManager.java
@@ -5,10 +5,10 @@ import javax.swing.JFrame;
 
 public class RobotManager {
 	public static void main(String[] args) {
-		final int COUNT = 5;
-		final int SIDE = 100;
+		final int COUNT = 8;
+		final int SIDE = 128;
 		
-		Robot robot = new Robot(200, 50);
+		Robot robot = new Robot(175, 50);
 		RobotListener listener = new SimpleRobotListener();
 		robot.setListener(listener);
 		
@@ -20,12 +20,6 @@ public class RobotManager {
 		RobotFrame rf = new RobotFrame(robot);
 		rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		rf.setVisible(true);
-
-		Robot[] robots = new Robot[10];
-		for (int i = 0; i < robots.length; i++) {
-			robots[i] = new Robot(i * 10, i * 4);
-			robots[i].printCoordinates();
-		}
 
 	}
 }
