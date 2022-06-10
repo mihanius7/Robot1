86243ec48721193812ba74fdc8e0a29444820cb6
Mihas Dainiak <mihanius@gmail.com>
2022-06-10 10:21:06 +0300

    Format output

--- src/edu/javacourse/robot/SimpleRobotListener.java
+++ src/edu/javacourse/robot/SimpleRobotListener.java
@@ -4,12 +4,12 @@ public class SimpleRobotListener implements RobotListener {
 
 	@Override
 	public void startMove(double x, double y) {
-		System.out.println("Robot started moving from (" + x + "; " + y + ")");
+		System.out.format("Robot started moving from (%.1f; %.1f) m.%n", x, y);
 	}
 
 	@Override
 	public void endMove(double x, double y) {
-		System.out.println("Robot finished moving at (" + x + "; " + y + ")");
+		System.out.format("Robot finished moving at (%.1f; %.1f) m.%n", x, y);
 	}
 
 }
